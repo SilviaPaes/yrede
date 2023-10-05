5c9d8e7281aa4f9a41339370ea9855059c14efbf
silviaPaes <silviadornellespaes@gmail.com>
2023-10-05 09:22:46 -0300

    tela de cadastro de usuario

---
+++ resources/views/cadastraUsuarios.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuários</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.7/dist/full.css" rel="stylesheet" type="text/css" />
+<script src="https://cdn.tailwindcss.com"></script>
+
+
+</head>
+<body>
+    
+
+
+<h1 class="w-full text-center text-xl font-bold">Cadastro de Usuários</h1>
+
+<form class="card-body max-w-md m-auto" action="" method="post">
+
+
+<div class="form-control">
+<label class="label">
+  <span class="font-bold">Usuário</span>
+</label>
+<input type="text" placeholder="usuario" class="input input-bordered" required>
+
+
+
+</div>
+
+
+<div class="form-control">
+<label class="label">
+  <span class="font-bold">Bio</span>
+</label>
+<input type="text" placeholder="Bio" class="input input-bordered" required>
+
+
+
+</div>
+
+<div class="form-control">
+<label class="label">
+  <span class="font-bold">Senha</span>
+</label>
+<input type="password" placeholder="Senha" class="input input-bordered" required>
+
+
+
+</div>
+
+<div class="form-control">
+<label class="label">
+  <span class="font-bold">Email</span>
+</label>
+<input type="text" placeholder="Email" class="input input-bordered" required>
+
+
+
+</div>
+
+
+
+
+</form>
+
+</body>
+</html>
