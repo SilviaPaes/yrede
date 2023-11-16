31f20f5d5ad2678a9ef2d6d0fccc65ba0748fcbe
= <=>
2023-11-16 11:10:34 -0300

    criação de posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+ public function getAuthPassword(){
+
+    return $this->password;
+ }
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
+
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,10 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $posts = Post::all();
+
+    return view('Inicial', compact('posts'));
 });
 
 Route::get('/teste', function () {
@@ -47,7 +53,7 @@ return "salvo com sucesso!";
 
 })->name('salva-usuario');
 
-Route::view('/login','login');
+Route::view('/login','login')->name('login');
 
 Route::post('/logar', function (Request $request) {
 // fazer login
@@ -57,12 +63,31 @@ $credentials = $request->validate([
     'password' => ['required'],
 ]);
 
-if (Auth::attempt($credentials)) {
+if (Auth::attempt(['email'=>$credentials['email'],'password' =>$credentials['password']])) {
     $request->session()->regenerate();
 
-    return "Logado com sucesso!!";
+return redirect()->intended('/');
+
+   // return "Logado com sucesso!!";
 }
 
 
  return "Erro ao logar!!! Usuário ou senha inválidos";
 });
+ Route::midleware(['auth'])->group(function() {
+
+    Route::view('/cria-Post','criaPost');
+
+    Route:: post('/salva-post', function(Request $request){
+        $post = new Post();
+
+       $post->user_id= Auth::id();
+       $post->mensagem = $request->mensagem;
+
+       $post->save();
+
+       return redirect('/');
+
+ });
+           
+});  
